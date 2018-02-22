ca761969f272b9f393533b6dbede603088a5fcf8
Xavier Riley <xavriley@hotmail.com>
2018-02-22 15:45:50 +0000

    WIP: graphviz output

--- lib/synthdef/graphviz.rb
+++ lib/synthdef/graphviz.rb
@@ -11,6 +11,9 @@ module Graphviz
   end
 
   def generate_node_info(sdef)
+    
+    output = ""
+
     sdef[:ugens].each do |ug|
       case ug[:ugen_name]
       when "Control"
@@ -20,6 +23,9 @@ module Graphviz
       else
         style = "\" shape=invhouse style=\"rounded, filled, bold\" fillcolor=black fontcolor=white ]; "
       end
+
+      output << Q{label = "#{ug[:ugen_name]}
+                  "}
     end
   end
 
